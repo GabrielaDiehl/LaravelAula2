37e9780b1a3631579cf90db27ffc76046f628f90
GabrielaDiehl <gabrielasd072@hotmail.com>
2018-08-27 14:58:09 -0300

    final aula 2908

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get ('/atividades/create', 'AtividadeController@create');
+Route::post('/atividades', "AtividadeController@store");
+
+
 Route::get('/atividades/{id}', 'AtividadeController@show');
 Route::get('/atividades', 'AtividadeController@index');
 
 
 Route::get('/mensagens/{id}', 'mensagensController@show');
 Route::get('/mensagens', 'mensagensController@index');
+
